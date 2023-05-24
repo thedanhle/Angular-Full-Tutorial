f4939d3992009234f37dedbf2e766eb240780522
Danh Le <danhthanhle@usf.edu>
2023-05-24 19:43:59 -0400

    Using bootstrap

--- angular-project/src/app/notification/notification.component.ts
+++ angular-project/src/app/notification/notification.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="notification-div">
+  template: `<div class="alert alert-success">
                 <p>This website uses cookies to provide better user experience.</p>
               </div>`,
   styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: orange; text-align: center;}", "p{font-size: 14px;}"]
