cae0f0c0944122f65d5c400555a2cff764ce3858
Danh Le <danhthanhle@usf.edu>
2023-05-24 17:38:15 -0400

    styke property in component

--- angular-project/src/app/notification/notification.component.ts
+++ angular-project/src/app/notification/notification.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div>
+  template: `<div class="notification-div">
                 <p>This website uses cookies to provide better user experience.</p>
               </div>`,
-  styleUrls: ['./notification.component.css']
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: orange; text-align: center;}", "p{font-size: 14px;}"]
 })
 export class NotificationComponent {
 
