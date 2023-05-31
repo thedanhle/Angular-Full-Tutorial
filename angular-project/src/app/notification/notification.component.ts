636d1384cf0c76393b74fb772569f74ecdd8483e
Danh Le <danhthanhle@usf.edu>
2023-05-30 21:42:39 -0400

    event binding

--- angular-project/src/app/notification/notification.component.ts
+++ angular-project/src/app/notification/notification.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template: `<div class="alert alert-success" [hidden]="displayNotification">
-                <p>This website uses cookies to provide better user experience.</p>
+                This website uses cookies to provide better user experience.
+                <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
               </div>`,
-  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: orange; text-align: center;}", "p{font-size: 14px;}"]
+  styles: ["div{margin: 10px 0px; padding: 10px 20px; text-align: center;}", "p{font-size: 14px;}", ".close{float: right; margin-top: -15px;}"]
 })
 export class NotificationComponent {
   displayNotification: boolean = false;
+
+  closeNotification(){
+    this.displayNotification = true;
+  }
 }
