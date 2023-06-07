2bc3877fa6b921389a8cb5d2734f124c4dc3f719
Danh Le <danhthanhle@usf.edu>
2023-06-07 18:42:39 -0400

    Two way data binding

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
@@ -20,7 +20,8 @@ import { SearchComponent } from './search/search.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
