21df54a767666ddd6b72602085f4a5c105975881
Danh Le <danhthanhle@usf.edu>
2023-06-07 22:29:32 -0400

    ngStyle

--- angular-project/src/app/products/products.component.ts
+++ angular-project/src/app/products/products.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
   products = [
-    {id:1, name: 'Analog Watch', price: '109', color: 'Black', available: 'Available', image: '/assets/products/AnalogWatch.jpg'},
-    {id:1, name: 'Washing Machine', price: '109', color: 'Black', available: 'Available', image: '/assets/products/WashingMachine.jpg'},
-    {id:1, name: 'iPhone 11', price: '109', color: 'Black', available: 'Available', image: '/assets/products/iPhone11.jpg'},
-    {id:1, name: 'Nintendo Switch', price: '109', color: 'Black', available: 'Available', image: '/assets/products/nintendoSwitch.jpg'},
-    {id:1, name: 'Beats by Dre', price: '109', color: 'Black', available: 'Available', image: '/assets/products/beatsByDre.jpg'}
+    {id:1, name: 'Analog Watch', price: '$109.99', color: 'Silver', available: 'Available', image: '/assets/products/AnalogWatch.jpg'},
+    {id:1, name: 'LG Washing Machine', price: '$499.99', color: 'White', available: 'Not Available', image: '/assets/products/WashingMachine.jpg'},
+    {id:1, name: 'iPhone 11', price: '$999.99', color: 'Silver', available: 'Available', image: '/assets/products/iPhone11.jpg'},
+    {id:1, name: 'Nintendo Switch', price: '$349.99', color: 'Black', available: 'Not Available', image: '/assets/products/nintendoSwitch.jpg'},
+    {id:1, name: 'Beats by Dre', price: '$99.99', color: 'Cream', available: 'Available', image: '/assets/products/beatsByDre.jpg'}
   ];
 }
